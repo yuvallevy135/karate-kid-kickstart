5d5aac08c1bffd97b2414e9bae9cfcd53609c8fe
Yuval Levy <yuvallevy@wix.com>
2022-02-20 14:06:01 +0200

    add ex 4 structure

---
+++ 5-Heroku/client/src/styles/styles.js
@@ -0,0 +1,26 @@
+export const styles = {
+    todoListItem: {
+        display: "flex",
+        "flex-direction": "row",
+        "align-items": "center",
+        padding: "15px",
+        "font-size": "1.5rem",
+        "border-bottom": "1px solid lightgrey",
+        "border-top": "1px solid lightgrey",
+    },
+    todoItem: {
+        "flex-grow": "1",
+        height: "30px",
+        border: "none",
+        outline: "0",
+        "font-size": "1.5rem",
+    },
+    completed: {
+        "text-decoration": "line-through",
+    },
+    checked: {
+        margin: "20px",
+        cursor: "pointer",
+    },
+
+}
---
+++ 5-Heroku/server/controllers/todoController.js
@@ -0,0 +1,45 @@
+const {
+    modelGetTodoById,
+    modelGetAllTodos,
+    modelPostTodo,
+    modelDeleteTodo,
+    modelUpdateTodo} = require('../models/todoModel')
+
+
+const getTodoById = (req, res) => {
+    modelGetTodoById(req.params.id, (todo) => {
+        res.status(200).json(todo)
+    });
+}
+const postTodo = (req, res) => {
+    modelPostTodo(req.body, () => {
+        res.sendStatus(201);
+    })
+}
+
+const getAllTodos = (req, res) => {
+    modelGetAllTodos(req, (todos) => {
+        console.log(todos);
+        res.status(200).json(todos)
+    })
+}
+
+const deleteTodo = (req, res) => {
+    modelDeleteTodo(req.params.id, () => {
+        res.sendStatus(200)
+    });
+}
+const updateTodo = (req, res) => {
+    modelUpdateTodo(req.body, () => {
+        res.sendStatus(200)
+    })
+}
+
+
+module.exports = {
+    getTodoById,
+    postTodo,
+    getAllTodos,
+    deleteTodo,
+    updateTodo
+}
