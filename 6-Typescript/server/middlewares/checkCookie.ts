09eb9951ea616ec93feab140a76f6780a4fbdfd6
Yuval Levy <yuvallevy@wix.com>
2022-03-02 17:37:33 +0200

    create middleware that checks if there is a cookie of the user

---
+++ 6-Typescript/server/middlewares/checkCookie.ts
@@ -0,0 +1,14 @@
+
+import { v4 as uuid } from 'uuid';
+import {Request, Response, NextFunction} from 'express'
+
+export const checkCookie = function (req: Request, res: Response, next: NextFunction) {
+    const userId: string = req.cookies['userId'];
+        
+    if (!userId) {
+        const userId: string = uuid();
+        res.cookie('userId', userId, {httpOnly: true, secure: true });
+    }
+    req.body.userId = userId
+    next()
+}
