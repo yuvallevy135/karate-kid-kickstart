fb1301e8c865a3c4657573d4d0628f48f3d65159
Yuval Levy <yuvallevy@wix.com>
2022-03-02 17:37:51 +0200

    create a function for the app

---
+++ 6-Typescript/server/app.ts
@@ -0,0 +1,18 @@
+import { TodoDAOMongo } from './models/TodoDAOMongo';
+import express, {Application} from 'express'
+import {todoRouter} from './routers/todoRouter'
+import cookieParser from 'cookie-parser';
+import {checkCookie} from './middlewares/checkCookie'
+require('dotenv').config();
+
+export default function(db: TodoDAOMongo) {
+    const app: Application = express();
+    app.use(express.static('public'));
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(checkCookie);
+    app.use('/api/todos', todoRouter(db));
+
+    return app;
+}
+    
